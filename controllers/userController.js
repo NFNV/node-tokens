100d48ac9eb7d3883064f38e92b848ce3bc089e6
Nicolás Valiconto <nvaliconto@outlook.com>
2022-11-07 19:36:51 -0300

    register post

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,51 @@
-const { authService } = require('../services');
+const { authService } = require("../services")
+
+//to use in register
+const User = require("../models/user")
 
 //login & register
-//fake login, it will never fail
 
+//fake login, it will never fail
 const login = (req, res) => {
-   res.status(200).send({ token: authService.createToken() });
+  res.status(200).send({ token: authService.createToken() })
 }
 
+// will recieve the parameters via post
 const register = (req, res) => {
+  const { email, password } = req.body
 
+  const newUser = new User({
+    email,
+    password,
+  })
+
+  //if email exists
+  //1st arg: if email existe
+  //2nd arg: cb with error & user. User means error, because already exists
+  User.findOne({ email: newUser.email }, (error, user) => {
+    if (error)
+      return res.status(500).send({ message: "Error creating user.", error })
+    if (user) return res.status(400).send({ message: "Email already in use." })
+    //saving with mongoose, using a function
+    newUser.save((error) => {
+      if (error)
+        res.status(400).send({ message: "Error creating user.", error })
+    })
+
+    //success msg & login with token
+    res.status(201).send({
+      message: "Successful registration",
+      token: authService.createToken(),
+    })
+  })
 }
 
 const sayHi = (req, res) => {
-    res.status(200).send('You are authenticated');
+  res.status(200).send("You are authenticated")
 }
 //exporting functions
 module.exports = {
-    login,
-    register,
-    sayHi,
+  login,
+  register,
+  sayHi,
 }
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //importing express
 const express = require("express")
 
+//importing mongoose
+const mongoose = require("mongoose")
+
 //importing dotenv
 const dotenv = require("dotenv")
 
@@ -19,7 +22,16 @@ dotenv.config()
 //middleware for '/api', then redirects to Routes
 app.use("/api", routes)
 
-//listening petitions, server up on port 3000
-app.listen(process.env.PORT, () => {
-  console.log(`Server up on port ${process.env.PORT}`)
-})
+//connecting to mongo
+mongoose.connect(
+  process.env.MONGO_DB,
+  { useNewUrlParser: true },
+  (error, result) => {
+    if (error) return console.log(`Error connecting to DB ${error}`)
+    console.log('Conection to the DB')
+    //listening petitions, server up on port 3000
+    app.listen(process.env.PORT, () => {
+      console.log(`Server up on port ${process.env.PORT}`)
+    })
+  }
+)
